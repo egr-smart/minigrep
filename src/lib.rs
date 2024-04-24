e1b022f65ab79c326fda1930e768e1e0ab89bc5b
Ed <esmartqq@gmail.com>
2024-04-24 15:37:15 +0800

    remove unneccessary variable in case insensitive search function

--- src/lib.rs
+++ src/lib.rs
@@ -56,10 +56,9 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
     contents
         .lines()
-        .filter(|line| line.to_lowercase().contains(&query))
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
 }
 
